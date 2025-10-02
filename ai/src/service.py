e3384a091cee0b54222d405e949ebbf61c5fb17b
Anaida <anaidashagininan@gmail.com>
2025-10-02 23:21:06 +0300

    add unzip

--- ai/src/service.py
+++ ai/src/service.py
@@ -2,6 +2,18 @@ import numpy as np
 from ai.src.app_service import AppService
 from ai.src.routers import router
 import json
+import os
+import subprocess
+
+
+def unzip_and_remove(zip_path):
+    folder = os.path.splitext(zip_path)[0]
+
+    os.makedirs(folder, exist_ok=True)
+
+    subprocess.run(["unzip", "-q", zip_path, "-d", folder], check=True)
+    os.remove(zip_path)
+    return folder
 
 
 service = AppService()
@@ -11,6 +23,7 @@ service = AppService()
 def predict(
         path: str,
         service: AppService = service):
+    path = unzip_and_remove(path)
     result = service.predict(path)
     return result
 
